4f95418df96cc8e3449ce662eaf95e5ab12caf56
Kovács Máté <kismate90@gmail.com>
2018-05-29 13:15:50 +0200

    Added methods to get and set the QUestion and evaluate an answer (presented as a List of Strings)

--- src/main/java/com/codecool/poop/model/coding/CodingAssignment.java
+++ src/main/java/com/codecool/poop/model/coding/CodingAssignment.java
@@ -2,12 +2,28 @@ package com.codecool.poop.model.coding;
 
 import com.codecool.poop.model.Assignment;
 import com.codecool.poop.model.Skills;
+import com.codecool.poop.model.User;
 
+import java.util.List;
 import java.util.Map;
 
 public class CodingAssignment extends Assignment{
 
+    private CodingQuestion question;
+
     public CodingAssignment(String name, String description, Map<Skills, Integer> expRewards, Integer codeCoinReward) {
         super(name, description, expRewards, codeCoinReward);
+    }
+
+    public String getQuestion(){
+        return question.toString();
+    }
+
+    public boolean evaluateSolution(List<String> answer){
+        return question.isAnswerCorrect(answer);
+    }
+
+    public void setQuestion(CodingQuestion question) {
+        this.question = question;
     }
 }
