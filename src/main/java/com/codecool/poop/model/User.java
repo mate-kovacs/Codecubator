9d8bcd67582cc433c0c92b12406fcc02abdcb4a5
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-05-29 12:31:59 +0200

    User class methods implemented

--- src/main/java/com/codecool/poop/model/User.java
+++ src/main/java/com/codecool/poop/model/User.java
@@ -1,4 +1,98 @@
 package com.codecool.poop.model;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class User {
+    private String username;
+    private String password;
+    private String email;
+    private int codeCoins;
+    private Date lastLogin;
+    private LocalDateTime registrationDate;
+    private List<Skills> skills = new ArrayList<>();
+    private List<Achievement> achievements = new ArrayList<>();
+
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        registrationDate = LocalDateTime.now();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCodeCoins() {
+        return codeCoins;
+    }
+
+    public void setCodeCoins(int codeCoins) {
+        this.codeCoins = codeCoins;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public List<Skills> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skills> skills) {
+        this.skills = skills;
+    }
+
+    public void addSkill(Skills skill) {
+        this.skills.add(skill);
+    }
+
+    public List<Achievement> getAchievements() {
+        return achievements;
+    }
+
+    public void setAchievements(List<Achievement> achievements) {
+        this.achievements = achievements;
+    }
+
+    public void addAchievement(Achievement achievement) {
+        this.achievements.add(achievement);
+    }
 }
