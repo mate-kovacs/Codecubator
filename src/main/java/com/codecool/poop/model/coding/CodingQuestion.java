4c24cd51a42a507fe20b84d41b6aca5607cacb1b
Kovács Máté <kismate90@gmail.com>
2018-05-29 14:19:38 +0200

    Added max points and updated check for correct solutions

--- src/main/java/com/codecool/poop/model/coding/CodingQuestion.java
+++ src/main/java/com/codecool/poop/model/coding/CodingQuestion.java
@@ -5,25 +5,41 @@ import java.util.List;
 public class CodingQuestion {
 
     private String instruction;
-    private List<String> questions;
+    private String question;
     private CodingAnswer answer;
 
-    public CodingQuestion(String instruction, List<String> questions) {
+    public CodingQuestion(String instruction, String question) {
         this.instruction = instruction;
-        this.questions = questions;
+        this.question = question;
+    }
+
+    public Integer correctSolutions(CodingAnswer currentAnswer){
+        return answer.matchingSolutions(currentAnswer);
+    }
+
+    public int getMaxPoints() {
+        return answer.getAnswers().size();
+    }
+
+    public CodingAnswer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(CodingAnswer answer) {
+        this.answer = answer;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public String getQuestions() {
+        return question;
     }
 
     @Override
     public String toString() {
         return "Your task: " + instruction +
-                "\n" + questions;
-    }
-
-    public boolean isAnswerCorrect(List<String> answer) {
-        return this.answer.isMatching(answer);
-    }
-
-    public void setAnswer(CodingAnswer answer) {
-        this.answer = answer;
+                "\n" + question;
     }
 }
