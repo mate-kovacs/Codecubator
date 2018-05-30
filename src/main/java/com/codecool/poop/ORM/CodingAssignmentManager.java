3fbce0e1f465027b9874263a3898dff32b7f469d
Kovács Máté <kismate90@gmail.com>
2018-05-30 11:28:33 +0200

    Coding Assignment can be added to Database (with assignment, question and answer objects)

--- src/main/java/com/codecool/poop/ORM/CodingAssignmentManager.java
+++ src/main/java/com/codecool/poop/ORM/CodingAssignmentManager.java
@@ -1,5 +1,12 @@
 package com.codecool.poop.ORM;
 
+import com.codecool.poop.model.assignments.coding.CodingAnswer;
+import com.codecool.poop.model.assignments.coding.CodingAssignment;
+import com.codecool.poop.model.assignments.coding.CodingQuestion;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
 public class CodingAssignmentManager extends DataManager {
     private static CodingAssignmentManager managerInstance = new CodingAssignmentManager();
 
@@ -8,5 +15,18 @@ public class CodingAssignmentManager extends DataManager {
     }
 
     private CodingAssignmentManager() {
+    }
+
+    public void addCodingAssignmentToDB(CodingAssignment assignment,
+                                    CodingQuestion question,
+                                    CodingAnswer answer){
+        EntityManager em = getEntityManagerFactory().createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(answer);
+        em.persist(question);
+        em.persist(assignment);
+        transaction.commit();
+        em.close();
     }
 }
