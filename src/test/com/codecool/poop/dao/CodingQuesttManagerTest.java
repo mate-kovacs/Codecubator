20c89a9530b0253ab999e1f54d801c21f3703dc4
Kovács Máté <kismate90@gmail.com>
2018-06-12 14:11:18 +0200

    Reworked Coding Quest Manager Tests to use the Dummy DB

--- src/test/com/codecool/poop/dao/CodingQuesttManagerTest.java
+++ src/test/com/codecool/poop/dao/CodingQuesttManagerTest.java
@@ -1,5 +1,6 @@
 package com.codecool.poop.dao;
 
+import com.codecool.poop.db_initializer.DummyDBInitializer;
 import com.codecool.poop.model.Skills;
 import com.codecool.poop.model.assignments.coding.CodingAnswer;
 import com.codecool.poop.model.assignments.coding.CodingAssignment;
@@ -20,74 +21,21 @@ class CodingQuesttManagerTest {
 
     @BeforeAll
     private static void setup() {
-        Map<Skills, Integer> reward = new HashMap<>();
-        reward.put(Skills.HTML_BASIC, 5);
-        reward.put(Skills.CSS_BASIC, 8);
-        Set<CodingQuestion> questions = new HashSet<>();
-        CodingQuestion question11 = new CodingQuestion("What?");
-        CodingQuestion question12 = new CodingQuestion("Why?");
-        Set<CodingQuestion> questions2 = new HashSet<>();
-        CodingQuestion question21 = new CodingQuestion("How?");
-        CodingQuestion question22 = new CodingQuestion("Who?");
+        UserManager userManager = new UserManager();
+        CodingQuestManager codingQuestManager = new CodingQuestManager();
+        QuizQuestManager quizQuestManager = new QuizQuestManager();
+        MasteryQuestManager masteryQuestManager = new MasteryQuestManager();
 
-        List<CodingAnswer> answers11 = new ArrayList<>();
-        answers11.add(new CodingAnswer("Th is", question11));
-        answers11.add(new CodingAnswer("Is", question11));
-        answers11.add(new CodingAnswer("Spar    ta", question11));
-        List<CodingAnswer> answers12 = new ArrayList<>();
-        answers12.add(new CodingAnswer("Just", question12));
-        answers12.add(new CodingAnswer("Be cause", question12));
-        List<CodingAnswer> answers21 = new ArrayList<>();
-        answers21.add(new CodingAnswer("Fourty", question21));
-        answers21.add(new CodingAnswer("Two", question21));
-        List<CodingAnswer> answers22 = new ArrayList<>();
-        answers22.add(new CodingAnswer("Me", question22));
+        DummyDBInitializer dummyDBInitializer = new DummyDBInitializer(
+                userManager,
+                quizQuestManager,
+                codingQuestManager,
+                masteryQuestManager);
+        dummyDBInitializer.initializeDummyDB();
 
-        questions.add(question11);
-        questions.add(question12);
-        questions2.add(question21);
-        questions2.add(question22);
-
-        CodingAssignment assignment = new CodingAssignment("Number one",
-                "Description",
-                reward,
-                3,
-                questions);
-        for (CodingQuestion question : questions) {
-            question.addAssignment(assignment);
-        }
-        CodingAssignment assignment2 = new CodingAssignment("Number two",
-                "Other",
-                reward,
-                2,
-                questions);
-        for (CodingQuestion question : questions2) {
-            question.addAssignment(assignment2);
-        }
-
-        CodingQuestManager manager = new CodingQuestManager();
-
-        testAssignmentID = manager.addCodingAssignmentToDB(assignment);
-        manager.addCodingAssignmentToDB(assignment2);
-
-        testQuestionID = manager.addCodingQuestionToDB(question11);
-        manager.addCodingQuestionToDB(question12);
-
-        for (CodingQuestion question : questions2) {
-            manager.addCodingQuestionToDB(question);
-        }
-
-        for (CodingAnswer answer : answers11) {
-            testAnswerID = manager.addCodingAnswerToDB(answer);
-        }
-        for (CodingAnswer answer : answers12) {
-            manager.addCodingAnswerToDB(answer);
-        }
-        for (CodingAnswer answer : answers21) {
-            manager.addCodingAnswerToDB(answer);
-        }
-        testAnswerID = manager.addCodingAnswerToDB(answers22.get(0));
-
+        testAssignmentID = 1;
+        testAnswerID = 8;
+        testQuestionID = 1;
 
     }
 
@@ -140,7 +88,6 @@ class CodingQuesttManagerTest {
         assertThrows(NullPointerException.class, () -> manager.getCodingAnswerByID(0).toString());
     }
 
-//    @Disabled
     @Test
     void test_get_all_coding_assignments_happy_path() {
 
@@ -148,12 +95,11 @@ class CodingQuesttManagerTest {
         assertEquals(2, manager.getAllCodingAssignments().size());
     }
 
-//    @Disabled
     @Test
     void test_get_all_coding_questions_happy_path() {
 
         CodingQuestManager manager = new CodingQuestManager();
-        assertEquals(4, manager.getAllCodingQuestions().size());
+        assertEquals(6, manager.getAllCodingQuestions().size());
     }
 
 }
