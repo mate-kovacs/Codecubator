3d7ee7a6aaa62a6d809d3fe9102ad62a22424a4a
Kovács Máté <kismate90@gmail.com>
2018-06-12 11:11:45 +0200

    Added login check to the Coding Assignment Page Servlet and fixed the signature

--- src/main/java/com/codecool/poop/controller/CodingAssignmentPage.java
+++ src/main/java/com/codecool/poop/controller/CodingAssignmentPage.java
@@ -1,11 +1,32 @@
 package com.codecool.poop.controller;
 
-import com.codecool.poop.model.assignments.coding.CodingAssignment;
+import com.codecool.poop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
-public class CodingAssignmentPage {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class CodingAssignmentPage extends HttpServlet implements LoginHandler {
 
     public CodingAssignmentPage(){
     }
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session;
+        session = request.getSession();
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        if (session.getId() == null){
+            response.sendRedirect("/");
+            return;
+        }
+
+        
+    }
 
 }
