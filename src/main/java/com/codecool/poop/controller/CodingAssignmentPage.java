8621b2ae9664cff0c8c831f4a4c6c28e2509b1bc
Kovács Máté <kismate90@gmail.com>
2018-06-12 11:04:01 +0200

    Created coding assignment page servlet with instantiation in InitializationServlet and with settings and URL mapping in web.xml

---
+++ src/main/java/com/codecool/poop/controller/CodingAssignmentPage.java
@@ -0,0 +1,11 @@
+package com.codecool.poop.controller;
+
+import com.codecool.poop.model.assignments.coding.CodingAssignment;
+
+public class CodingAssignmentPage {
+
+    public CodingAssignmentPage(){
+    }
+
+
+}
--- src/main/java/com/codecool/poop/controller/controller_manager/InitializationServlet.java
+++ src/main/java/com/codecool/poop/controller/controller_manager/InitializationServlet.java
@@ -1,5 +1,6 @@
 package com.codecool.poop.controller.controller_manager;
 
+import com.codecool.poop.controller.CodingAssignmentPage;
 import com.codecool.poop.controller.Index;
 import com.codecool.poop.controller.Login;
 import com.codecool.poop.controller.Registration;
@@ -16,10 +17,12 @@ public class InitializationServlet  extends HttpServlet{
         Login servletLogin = new Login(userManager);
         Registration servletRegistration = new Registration(userManager);
         Index servletIndex = new Index();
+        CodingAssignmentPage servletCodingAssignment = new CodingAssignmentPage();
 
         getServletContext().setAttribute("servletIndex", servletIndex);
         getServletContext().setAttribute("servletLogin", servletLogin);
         getServletContext().setAttribute("servletRegistration", servletRegistration);
+        getServletContext().setAttribute("servletCodingAssignment", servletCodingAssignment);
 
     }
 }
