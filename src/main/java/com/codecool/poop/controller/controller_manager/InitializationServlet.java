cbbe66e6ace1ea4323b89e3ed8e029cac2b8ab04
Kovács Máté <kismate90@gmail.com>
2018-06-12 14:04:09 +0200

    Added dummy database creation to Initialization Servlet (which runs at the very beginning of the code, and injects the dummy db initializer with the necessary dao dependencies)

--- src/main/java/com/codecool/poop/controller/controller_manager/InitializationServlet.java
+++ src/main/java/com/codecool/poop/controller/controller_manager/InitializationServlet.java
@@ -3,7 +3,11 @@ package com.codecool.poop.controller.controller_manager;
 import com.codecool.poop.controller.Index;
 import com.codecool.poop.controller.Login;
 import com.codecool.poop.controller.Registration;
+import com.codecool.poop.dao.CodingQuestManager;
+import com.codecool.poop.dao.MasteryQuestManager;
+import com.codecool.poop.dao.QuizQuestManager;
 import com.codecool.poop.dao.UserManager;
+import com.codecool.poop.db_initializer.DummyDBInitializer;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +16,9 @@ public class InitializationServlet  extends HttpServlet{
     public void init() throws ServletException
     {
         UserManager userManager = new UserManager();
+        CodingQuestManager codingQuestManager = new CodingQuestManager();
+        QuizQuestManager quizQuestManager = new QuizQuestManager();
+        MasteryQuestManager masteryQuestManager = new MasteryQuestManager();
 
         Login servletLogin = new Login(userManager);
         Registration servletRegistration = new Registration(userManager);
@@ -21,5 +28,11 @@ public class InitializationServlet  extends HttpServlet{
         getServletContext().setAttribute("servletLogin", servletLogin);
         getServletContext().setAttribute("servletRegistration", servletRegistration);
 
+        DummyDBInitializer dummyDBInitializer = new DummyDBInitializer(
+                userManager,
+                quizQuestManager,
+                codingQuestManager,
+                masteryQuestManager);
+        dummyDBInitializer.initializeDummyDB();
     }
 }
