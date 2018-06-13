1e1b85ee04773117ae3f48acbdc6f73894680562
Kovács Máté <kismate90@gmail.com>
2018-06-14 01:41:57 +0200

    Created basic layout for the coding questions (with input fields to fill in the blanks) and added request for the first question of the given assignment

--- src/main/webapp/static/js/coding_assignment.js
+++ src/main/webapp/static/js/coding_assignment.js
@@ -5,14 +5,15 @@ function take_assignment_listener() {
         assignment_start.style.visibility = "hidden";
         let question_list = document.getElementById("question-list");
         question_list.style.visibility = "visible";
+
+        let assignment_id = document.getElementById("assignment_id").getAttribute("data-assignment_id");
+        get_next_question(0, assignment_id);
     })
 }
 
 function submit_question_listener() {
-    let coding_questions = document.getElementsByClassName("coding-question");
-    console.log(coding_questions);
-    for (let current_question of coding_questions){
-        console.log(current_question);
+    let coding_questions = document.getElementsByClassName("submit-question-button");
+    for (let current_question of coding_questions) {
         current_question.addEventListener("click", function () {
             get_next_question(2, 1);
         })
@@ -33,6 +34,27 @@ function get_next_question(question_id, assignment_id) {
             } else {
                 console.log("next question");
                 console.log(response);
+                let coding_question_id = document.getElementById("question_id");
+                coding_question_id.setAttribute("data-question_id", response.question_id);
+
+                let question_parts = response.question_text.split("$");
+                let answer_ids = response.answer_ids;
+                let coding_question_body = document.getElementById("question_holder");
+
+                let html_string =
+                    "<div class='question_part'>" +
+                    question_parts[0] +
+                    "</div>";
+                for (let i = 0; i < answer_ids.length; i++) {
+                    html_string +=
+                        "<input class='answer_part' type='text' id='" + answer_ids[i] + "'>" +
+                        "<div class='question_part'>" +
+                        question_parts[i + 1] +
+                        "</div>"
+                }
+
+                coding_question_body.innerHTML = html_string;
+
             }
         },
     });
