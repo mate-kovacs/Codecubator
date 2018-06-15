0b956e05c468f79ca255ae97be2988d56c9da604
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-06-15 09:01:34 +0200

    Remove music, fix some design issues

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -4,16 +4,6 @@ index = {
         this.tbcArrowSlide();
     },
 
-    tbcArrowSlide: function () {
-        let music = document.getElementById("music");
-        music.play();
-        let tbc_image = document.getElementById("tbc");
-        setTimeout(function () {
-            tbc_image.style.opacity = "1";
-            tbc_image.style.marginRight = "68%";
-        }, 3500);
-    },
-
     changeColorOfUsername: function () {
         let welcomeText = document.getElementById("welcome_h1");
         let text = welcomeText.innerText.split(' ');
