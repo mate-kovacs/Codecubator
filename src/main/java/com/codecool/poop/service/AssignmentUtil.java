1baf7ed4c75079443e23011a60c0dd4dabb96c14
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-06-26 13:25:32 +0200

    Created an assignment util class

---
+++ src/main/java/com/codecool/poop/service/AssignmentUtil.java
@@ -0,0 +1,20 @@
+package com.codecool.poop.service;
+
+import com.codecool.poop.model.User;
+
+import javax.servlet.http.HttpSession;
+
+public class AssignmentUtil {
+
+    private boolean isUserDead(User user, HttpSession session, boolean correctAnswer) {
+        boolean death = false;
+
+        if (!correctAnswer) {
+            userLoseHealth(session);
+            if (user.getHealth() == 0) {
+                death = true;
+            }
+        }
+        return death;
+    }
+}
