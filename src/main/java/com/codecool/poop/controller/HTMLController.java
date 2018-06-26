7574a5f9f1c424bde629a3df657369ae599fc4ef
Kristóf Egressy <egressyk@gmail.com>
2018-06-26 17:29:50 +0200

    User registration and login controllers

--- src/main/java/com/codecool/poop/controller/HTMLController.java
+++ src/main/java/com/codecool/poop/controller/HTMLController.java
@@ -1,39 +1,40 @@
 package com.codecool.poop.controller;
 
-import com.codecool.poop.dao.UserManager;
+
+import com.codecool.poop.service.SessionService;
+import com.codecool.poop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @Controller
 public class HTMLController implements LoginHandler{
 
     @Autowired
-    private UserManager userManager;
+    private UserService userService;
 
-    @GetMapping(value = "/")
+    @Autowired
+    private SessionService sessionService;
+
+    @GetMapping(value = "/login")
     public String loginPage() {
         return "login";
     }
+
 
     @GetMapping(value = "/registration")
     public String registrationPage() {
         return "registration";
     }
 
-    @GetMapping(value = "/index")
-    public String indexPage(HttpServletRequest request, Model model) throws IOException {
-        HttpSession session;
-        session = request.getSession();
-        if (!isUserLoggedIn(session)) {
-            return "redirect:/";
+    @GetMapping(value = "/")
+    public String indexPage(Model model) {
+        if (sessionService.getCurrentUser() == null) {
+            return "redirect:/login";
         }
-        model.addAttribute("user_name", "Pityu");
+        model.addAttribute("user_name", sessionService.getCurrentUser().getUsername());
         return "index";
     }
 }
--- src/main/java/com/codecool/poop/controller/Login.java
+++ src/main/java/com/codecool/poop/controller/Login.java
@@ -1,25 +1,5 @@
 package com.codecool.poop.controller;
 
-import com.codecool.poop.dao.UserManager;
-
-import com.codecool.poop.model.User;
-import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-
-import javax.persistence.NoResultException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 //@Controller
 //public class Login {
 //
