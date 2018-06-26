e19ee09e465d927829838a003940ee3cb9428aa9
Tibor Vlasics <tibor.vlasics@gmail.com>
2018-06-26 12:55:34 +0200

    Add reward to user

--- src/main/java/com/codecool/poop/service/UserService.java
+++ src/main/java/com/codecool/poop/service/UserService.java
@@ -1,9 +1,14 @@
 package com.codecool.poop.service;
 
+import com.codecool.poop.model.Skills;
 import com.codecool.poop.model.User;
+import com.codecool.poop.model.assignments.Assignment;
 import com.codecool.poop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.NoResultException;
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -18,6 +23,30 @@ public class UserService {
             return false;
         }
         return true;
+    }
+
+    public User getUserByName(String name) throws NoResultException {
+        return userRepository.findUserByUsername(name);
+    }
+
+    public void addRewardToUser(User user, Assignment assignment) {
+        addCoinRewardToUser(user, assignment);
+        addSkillRewardToUser(user, assignment);
+    }
+
+    private void addCoinRewardToUser(User user, Assignment assignment) {
+        int codeCoinReward = assignment.getCodeCoinReward();
+        int userCoins = user.getCodeCoins() + codeCoinReward;
+        user.setCodeCoins(userCoins);
+    }
+
+    private void addSkillRewardToUser(User user, Assignment assignment) {
+        Map<Skills, Integer> expRewards = assignment.getExpRewards();
+        for (Map.Entry<Skills, Integer> entry : expRewards.entrySet()) {
+            Skills skill = entry.getKey();
+            Integer value = entry.getValue();
+            user.addXpValueToSkill(skill, value);
+        }
     }
 
 
