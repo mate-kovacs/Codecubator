32c3b2e63e7229c8f51b8a08360a370d3f50a68f
Tibor Vlasics <tibor.vlasics@gmail.com>
2018-06-26 13:01:42 +0200

    MasteryQuest service and repository

---
+++ src/main/java/com/codecool/poop/repository/MasteryQuestRepository.java
@@ -0,0 +1,9 @@
+package com.codecool.poop.repository;
+
+import com.codecool.poop.model.assignments.mastery.MasteryAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MasteryQuestRepository extends JpaRepository<MasteryAssignment, Long> {
+
+
+}
---
+++ src/main/java/com/codecool/poop/service/MasteryQuestService.java
@@ -0,0 +1,16 @@
+package com.codecool.poop.service;
+
+import com.codecool.poop.repository.MasteryQuestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MasteryQuestService {
+
+    @Autowired
+    private MasteryQuestRepository masteryQuestRepository;
+
+
+
+}
