be6c2233105df53fdf109a715ee8775e20cd7a93
Kovács Máté <kismate90@gmail.com>
2018-06-27 09:56:58 +0200

    Updated API controller to use Services

--- src/main/java/com/codecool/poop/controller/APIAssignmentController.java
+++ src/main/java/com/codecool/poop/controller/APIAssignmentController.java
@@ -1,13 +1,15 @@
 package com.codecool.poop.controller;
 
-import com.codecool.poop.dao.CodingQuestManager;
-import com.codecool.poop.dao.QuizQuestManager;
-import com.codecool.poop.dao.UserManager;
 import com.codecool.poop.model.User;
 import com.codecool.poop.model.assignments.Assignment;
 import com.codecool.poop.model.assignments.coding.CodingQuestion;
 import com.codecool.poop.model.assignments.quiz.QuizAnswer;
 import com.codecool.poop.model.assignments.quiz.QuizQuestion;
+import com.codecool.poop.service.SessionService;
+import com.codecool.poop.service.coding_services.CodingAssignmentService;
+import com.codecool.poop.service.coding_services.CodingQuestionService;
+import com.codecool.poop.service.quiz_services.QuizAssignmentService;
+import com.codecool.poop.service.quiz_services.QuizQuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,32 +18,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.*;
 
 @RestController
 public class APIAssignmentController {
 
     @Autowired
-    private CodingQuestManager codingQuestManager;
+    private CodingAssignmentService codingAssignmentService;
     @Autowired
-    private QuizQuestManager quizQuestManager;
+    private CodingQuestionService codingQuestionService;
     @Autowired
-    private UserManager userManager;
+    private QuizAssignmentService quizAssignmentService;
+    @Autowired
+    private QuizQuestionService quizQuestionService;
     @Autowired
     private AssignmentUtility utility;
+    @Autowired
+    private SessionService sessionService;
 
     @RequestMapping(value = "/coding-assignment", method = RequestMethod.POST)
-    public ResponseEntity<String> nextCodingQuetion(HttpServletRequest request,
-                                              @RequestParam("assignment_id") Integer assignmentID,
-                                              @RequestParam("question_id") Integer questionID) {
-        HttpSession session = request.getSession();
-        List<CodingQuestion> questionList = codingQuestManager.getCodingAssignemntByID(assignmentID).getQuestions();
+    public ResponseEntity<String> nextCodingQuetion(@RequestParam("assignment_id") Integer assignmentID,
+                                                    @RequestParam("question_id") Integer questionID) {
+        List<CodingQuestion> questionList = codingAssignmentService.getCodingAssignmentById(assignmentID).getQuestions();
 
         if (utility.isLastCodingQuestion(questionID, questionList)) {
-            Assignment assignment = codingQuestManager.getCodingAssignemntByID(assignmentID);
-            Map<String, Object> assignmentEvaluation = utility.getAssignmentEvaluation(assignment, session);
+            Assignment assignment = codingAssignmentService.getCodingAssignmentById(assignmentID);
+            Map<String, Object> assignmentEvaluation = utility.getAssignmentEvaluation(assignment);
             return new ResponseEntity(assignmentEvaluation, HttpStatus.OK);
         } else {
             Map<String, Object> nextQuestionData = utility.getNextCodingQuestionData(questionID, questionList);
@@ -50,15 +52,13 @@ public class APIAssignmentController {
     }
 
     @RequestMapping(value = "/quiz-assignment", method = RequestMethod.POST)
-    public ResponseEntity<String> nextQuizQuetion(HttpServletRequest request,
-                                              @RequestParam("assignment_id") Integer assignmentID,
-                                              @RequestParam("question_id") Integer questionID) {
-        HttpSession session = request.getSession();
-        List<QuizQuestion> questionList = quizQuestManager.getQuizAssignemntByID(assignmentID).getQuestions();
+    public ResponseEntity<String> nextQuizQuetion(@RequestParam("assignment_id") Integer assignmentID,
+                                                  @RequestParam("question_id") Integer questionID) {
+        List<QuizQuestion> questionList = quizAssignmentService.getQuizAssignmentById(assignmentID).getQuestions();
 
         if (utility.isLastQuizQuestion(questionID, questionList)) {
-            Assignment assignment = quizQuestManager.getQuizAssignemntByID(assignmentID);
-            Map<String, Object> assignmentEvaluation = utility.getAssignmentEvaluation(assignment, session);
+            Assignment assignment = quizAssignmentService.getQuizAssignmentById(assignmentID);
+            Map<String, Object> assignmentEvaluation = utility.getAssignmentEvaluation(assignment);
             return new ResponseEntity(assignmentEvaluation, HttpStatus.OK);
         } else {
             Map<String, Object> nextQuestionData = utility.getNextQuizQuestionData(questionID, questionList);
@@ -67,39 +67,33 @@ public class APIAssignmentController {
     }
 
     @RequestMapping(value = "/coding-answer", method = RequestMethod.POST)
-    public ResponseEntity<String> evaluateCodingAnswer(HttpServletRequest request,
-                                              @RequestParam("answers[]") String answers[],
-                                              @RequestParam("question_id") Integer questionID) {
+    public ResponseEntity<String> evaluateCodingAnswer(@RequestParam("answers[]") String answers[],
+                                                       @RequestParam("question_id") Integer questionID) {
 
         List<String> answerTexts = new ArrayList<>(Arrays.asList(answers));
-        HttpSession session = request.getSession();
-        Map<String, Object> userData = (Map) session.getAttribute("user");
-        User user = userManager.getUserByName(userData.get("user_name").toString());
+        User user = sessionService.getCurrentUser();
 
 
-        CodingQuestion question = codingQuestManager.getCodingQuestionByID(questionID);
+        CodingQuestion question = codingQuestionService.getCodingQuestionById(questionID);
         int numberOfCorrectAnswers = question.checkSolution(answerTexts);
         boolean correctAnswer = numberOfCorrectAnswers == question.getMaxPoints();
         boolean death = utility.isUserDead(user, correctAnswer);
 
-        utility.savePointsToSession(session, numberOfCorrectAnswers);
+        utility.savePointsToSession(numberOfCorrectAnswers);
 
         Map<String, Object> answerEvaluation = utility.getAnswerEvaluation(correctAnswer, death);
         return new ResponseEntity(answerEvaluation, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/quiz-answer", method = RequestMethod.POST)
-    public ResponseEntity<String> evaluateQuizAnswer(HttpServletRequest request,
-                                                    @RequestParam("answers[]") String answers[],
-                                                    @RequestParam("question_id") Integer questionID) {
+    public ResponseEntity<String> evaluateQuizAnswer(@RequestParam("answers[]") String answers[],
+                                                     @RequestParam("question_id") Integer questionID) {
 
         List<String> answerTexts = new ArrayList<>(Arrays.asList(answers));
-        HttpSession session = request.getSession();
-        Map<String, Object> userData = (Map) session.getAttribute("user");
-        User user = userManager.getUserByName(userData.get("user_name").toString());
+        User user = sessionService.getCurrentUser();
 
 
-        QuizQuestion question = quizQuestManager.getQuizQuestionByID(questionID);
+        QuizQuestion question = quizQuestionService.getQuizQuestionById(questionID);
         List<QuizAnswer> correctAnswerList = question.getQuizAnswers();
         int numberOfCorrectAnswers = 0;
         for (int i = 0; i < correctAnswerList.size(); i++) {
@@ -112,7 +106,7 @@ public class APIAssignmentController {
         boolean correctAnswer = numberOfCorrectAnswers == question.getMaxPoints();
         boolean death = utility.isUserDead(user, correctAnswer);
 
-        utility.savePointsToSession(session, numberOfCorrectAnswers);
+        utility.savePointsToSession(numberOfCorrectAnswers);
 
         Map<String, Object> answerEvaluation = utility.getAnswerEvaluation(correctAnswer, death);
         return new ResponseEntity(answerEvaluation, HttpStatus.OK);
