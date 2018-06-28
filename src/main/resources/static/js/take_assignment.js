ba7dd4486be96dfb020809c730960537649f0598
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-06-28 14:24:22 +0200

    Fix the takeassignment javascript for the rooms

--- src/main/resources/static/js/take_assignment.js
+++ src/main/resources/static/js/take_assignment.js
@@ -1,18 +1,22 @@
 
 
 function takeAssignmentListener() {
-    let buttons = document.getElementsByClassName("take-assignment");
-    console.log(buttons);
-    for (button of buttons) {
-        console.log(button);
-        button.addEventListener("click", function() {
-            let id = this.dataset.id;
-            let uri = location.pathname.substr(1);
+    let quizzes = document.getElementsByClassName("quiz");
+    console.log(quizzes);
+    for (quiz of quizzes) {
+        quiz.addEventListener("click", function() {
+            let id = this.id;
             console.log(id);
-            console.log(uri);
-            uri = uri.slice(0, -1);
-            console.log(uri);
-            document.location.href = "/" + uri + "?assignment_id=" + id;
+            document.location.href = "/quiz-assignment?assignment_id=" + id;
+        });
+    }
+    let codings = document.getElementsByClassName("coding");
+    console.log(codings);
+    for (coding of codings) {
+        coding.addEventListener("click", function() {
+            let id = this.id;
+            console.log(id);
+            document.location.href = "/coding-assignment?assignment_id=" + id;
         });
     }
 }
