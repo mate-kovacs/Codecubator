1e3898ad190904ad9bfa833e0c380cb4dc1f1946
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-06-28 14:32:32 +0200

    Fix the data attributes in html tags of assignments

--- src/main/resources/static/js/take_assignment.js
+++ src/main/resources/static/js/take_assignment.js
@@ -5,18 +5,14 @@ function takeAssignmentListener() {
     console.log(quizzes);
     for (quiz of quizzes) {
         quiz.addEventListener("click", function() {
-            let id = this.id;
-            console.log(id);
-            document.location.href = "/quiz-assignment?assignment_id=" + id;
+            document.location.href = "/quiz-assignment?assignment_id=" + this.dataset.id;
         });
     }
     let codings = document.getElementsByClassName("coding");
     console.log(codings);
     for (coding of codings) {
         coding.addEventListener("click", function() {
-            let id = this.id;
-            console.log(id);
-            document.location.href = "/coding-assignment?assignment_id=" + id;
+            document.location.href = "/coding-assignment?assignment_id=" + this.dataset.id;
         });
     }
 }
